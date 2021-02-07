41169bb843a05112a6668b15ad048f62a9b2a48d
tomini18 <tomini18@hotmail.com>
2021-02-07 21:23:54 +0100

    added dragn and drop in create devits

--- components/Devit/index.js
+++ components/Devit/index.js
@@ -1,7 +1,14 @@
 import Avatar from 'components/Avatar';
 import useTimeAgo from 'hooks/useTimeAgo';
 
-export default function Devit({ avatar, userName, content, createdAt, id }) {
+export default function Devit({
+  avatar,
+  userName,
+  content,
+  createdAt,
+  img,
+  id,
+}) {
   const timeago = useTimeAgo(createdAt);
   return (
     <>
@@ -16,6 +23,7 @@ export default function Devit({ avatar, userName, content, createdAt, id }) {
             <date>{timeago}</date>
           </header>
           <p>{content}</p>
+          {img && <img src={img} />}
         </section>
       </article>
       <style jsx>{`
@@ -23,6 +31,13 @@ export default function Devit({ avatar, userName, content, createdAt, id }) {
           border-bottom: 1px solid #eee;
           display: flex;
           padding: 10px 15px;
+        }
+
+        img {
+          border-radius: 10px;
+          height: auto;
+          margin-top: 10px;
+          width: 100%;
         }
 
         div {
--- pages/compose/tweet/index.js
+++ pages/compose/tweet/index.js
@@ -1,10 +1,12 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AppLayout from 'components/AppLayout';
 import Button from 'components/Button';
 import useUser from 'hooks/useUser';
+import Head from 'next/head';
 
-import { addDevit } from 'firebase/client';
+import { addDevit, uploadImage } from 'firebase/client';
 import { useRouter } from 'next/router';
+import Avatar from 'components/Avatar';
 
 const COMPOSE_STATES = {
   USER_NOT_KNOWN: 0,
@@ -13,11 +15,36 @@ const COMPOSE_STATES = {
   ERROR: -1,
 };
 
+const DRAG_IMAGE_STATES = {
+  ERROR: -1,
+  NONE: 0,
+  DRAG_OVER: 1,
+  UPLOADING: 2,
+  COMPLETE: 3,
+};
+
 export default function ComposeTweet() {
-  const user = useUser();
-  const [status, setStatus] = useState(COMPOSE_STATES.USER_NOT_KNOWN);
   const [message, setMessage] = useState('');
+  const [status, setStatus] = useState(COMPOSE_STATES.USER_NOT_KNOWN);
+
+  const [drag, setDrag] = useState(DRAG_IMAGE_STATES.NONE);
+  const [task, setTask] = useState(null);
+  const [imgURL, setImgURL] = useState(null);
+
+  const user = useUser();
   const router = useRouter();
+
+  useEffect(() => {
+    if (task) {
+      const onProgress = () => {};
+      const onError = () => {};
+      const onComplete = () => {
+        task.snapshot.ref.getDownloadURL().then(setImgURL);
+      };
+
+      task.on('state_changed', onProgress, onError, onComplete);
+    }
+  }, [task]);
 
   const handleChange = (event) => {
     const { value } = event.target;
@@ -32,6 +59,7 @@ export default function ComposeTweet() {
       content: message,
       userId: user.uid,
       userName: user.username,
+      img: imgURL,
     })
       .then(() => router.push('/home'))
       .catch((_err) => {
@@ -41,32 +69,112 @@ export default function ComposeTweet() {
 
   const isButtonDisabled = !message.length || status === COMPOSE_STATES.LOADING;
 
+  const handleDragEnter = (e) => {
+    e.preventDefault();
+    setDrag(DRAG_IMAGE_STATES.DRAG_OVER);
+  };
+
+  const handleDragLeave = (e) => {
+    e.preventDefault();
+    setDrag(DRAG_IMAGE_STATES.NONE);
+  };
+
+  const handleDragDrop = (e) => {
+    e.preventDefault();
+    setDrag(DRAG_IMAGE_STATES.NONE);
+
+    const file = e.dataTransfer.files[0];
+    const task = uploadImage(file);
+    setTask(task);
+  };
+
   return (
     <>
       <AppLayout>
-        <form onSubmit={handleSubmit}>
-          <textarea
-            placeholder="¿Qué está pasando?"
-            value={message}
-            onChange={handleChange}
-          ></textarea>
-          <div>
-            <Button disabled={isButtonDisabled}>Devitear</Button>
-          </div>
-        </form>
+        <Head>
+          <title>Crear un Devit / Devter</title>
+        </Head>
+        <section className="form-container">
+          <section className="avatar-container">
+            {user && <Avatar src={user.avatar} />}
+          </section>
+          <form onSubmit={handleSubmit}>
+            <textarea
+              placeholder="¿Qué está pasando?"
+              onDragEnter={handleDragEnter}
+              onDragLeave={handleDragLeave}
+              onDrop={handleDragDrop}
+              value={message}
+              onChange={handleChange}
+            ></textarea>
+            {imgURL && (
+              <section className="remove-img">
+                <button onClick={() => setImgURL(null)}>x</button>
+                <img src={imgURL} />
+              </section>
+            )}
+            <div>
+              <Button disabled={isButtonDisabled}>Devitear</Button>
+            </div>
+          </form>
+        </section>
       </AppLayout>
       <style jsx>{`
         div {
           padding: 15px;
         }
 
-        textarea {
+        .avatar-container {
+          padding-top: 20px;
+          margin-left: 10px;
+        }
+
+        button {
+          background: rgba(0, 0, 0, 0.3);
           border: 0;
-          padding: 15px;
+          border-radius: 999px;
+          color: #fff;
+          font-size: 24px;
+          width: 32px;
+          height: 32px;
+          top: 15px;
+          position: absolute;
+          right: 15px;
+        }
+
+        section {
+          position: relative;
+        }
+
+        .form-container {
+          align-items: flex-start;
+          display: flex;
+        }
+
+        .remove-img {
+          position: relative;
+        }
+
+        form {
+          padding: 10px;
+        }
+
+        img {
+          border-radius: 10px;
+          height: auto;
+          width: 100%;
+        }
+
+        textarea {
+          border: ${drag === DRAG_IMAGE_STATES.DRAG_OVER
+            ? '3px dashed #09f'
+            : '3px solid transparent'};
+          border-radius: 10px;
+          font-size: 21px;
           min-height: 200px;
+          padding: 15px;
           outline: 0;
           resize: none;
-          font-size: 21px;
           width: 100%;
         }
       `}</style>
